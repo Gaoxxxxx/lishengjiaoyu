7d7d547ed37455229e768eabc529bd015c575a71
Gaoxxxxx <13100818206@163.com>
2019-11-04 14:43:13 +0800

    Header

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <template>
   <div id="app">
+    <div id='container'>
+      <common-header></common-header>
+    </div>
   </div>
 </template>
 
 <script>
-
+import CommonHeader from './components/CommonHeader';
 export default {
   name: 'app',
   components: {
-    
+    CommonHeader
   }
 }
 </script>
@@ -18,8 +21,11 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  /* text-align: center; */
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 20px;
+}
+#container{
+margin: 0 100px
 }
 </style>
