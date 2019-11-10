8b3f457c4a9894a41d95bc2fdd24f32a8cd5a00e
Gaoxxxxx <13100818206@163.com>
2019-11-10 15:48:59 +0800

    header

---
+++ lishengjiaoyu/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <common-header></common-header>
+  </div>
+</template>
+<script>
+import CommonHeader from './components/CommonHeader.vue';
+
+export default {
+  components: {
+    CommonHeader,
+  },
+};
+</script>
+<style lang="scss">
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
---
+++ lishengjiaoyu/src/components/CommonHeader.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="header">
+    <img src="../image/lsjy.png" alt="" class='logo'>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style lang="scss">
+  .header{
+    display: block;
+    width: 100%;
+    height:80px;
+    box-shadow: 0px 1px 0px #888888;
+    .logo{
+      width: 200px;
+      height: 80px;
+    }
+  }
+</style>
